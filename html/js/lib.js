dd4c95e317ef9f8c2059d798a70519128da55cb2
prasathj0902 <prasathj10@gmail.com>
2018-06-10 00:21:13 +0530

    update

--- html/js/lib.js
+++ html/js/lib.js
@@ -1,5 +1,14 @@
 $(function(){
+	 $(".timeperiod > span").on("click",function(){
+        $(this).addClass("active").siblings().removeClass("active");
+       console.log("12");      
+       
+   });
     $(".switch-toggle > span").on("click", function(){
-        $(this).addClass("active").siblings().removeClass("active")
-    });
+        $(this).addClass("active").siblings().removeClass("active");
+         $(".timeperiod >span").trigger("click");
+       console.log("12");
+       });
+    
 });
+ 
